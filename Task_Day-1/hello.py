cfb13bfe6e4bf9fc8f14b820900e15f555e867bc
mb-tousif <meta.block.tousif@gmail.com>
2023-02-15 21:07:09 +0600

    Problem solving

--- Task_Day-1/hello.py
+++ Task_Day-1/hello.py
@@ -1,1 +1,11 @@
 print("Hello World");
+
+# How check the odd or even number
+
+num = int(input("Enter a number"))
+
+if(num%2 == 0):
+    print("This is a even Number")
+else:
+    print("This is odd Number")
+
