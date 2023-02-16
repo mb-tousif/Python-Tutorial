744a4279d2ca355467dad5c17d0572b4048bd26d
mb-tousif <meta.block.tousif@gmail.com>
2023-02-16 21:51:18 +0600

    Python Data Types

---
+++ Task_Day-1/DataTypes.py
@@ -0,0 +1,28 @@
+# String
+name="Tousif"
+print(name);
+print(type(name));
+# Number
+num=10;
+print(num);
+print(type(num));
+# Boolean
+isTrue=True;
+print(isTrue);
+print(type(isTrue));
+# List
+list=[1,2,3,4,5];
+print(list);
+print(type(list));
+# Tuple
+tuple=(1,2,3,4,5);
+print(tuple);
+print(type(tuple));
+# Set
+set={1,2,3,4,5};
+print(set);
+print(type(set));
+# Dictionary
+dict={"name":"Tousif","age":25};
+print(dict);
+print(type(dict));
