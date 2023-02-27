7d975904b44e97bbdf3b95c00aa1d8d808b36c48
mb-tousif <meta.block.tousif@gmail.com>
2023-02-28 00:30:45 +0600

    Printing Pattern in Python 🍇

--- Day-4/NestedLoops.py
+++ Day-4/NestedLoops.py
@@ -12,3 +12,26 @@ for i in range(5, 8):
     for j in range(1, 11):
         print(i, 'x', j, '=', i*j)
     print()
+
+# Write a program to print pattern in Python
+for i in range(1, 5):
+    for j in range(1, i+1):
+        print('🐍', end=' ')
+    print()
+
+# Write a program to print a pattern where a to z is printed in the following format:
+# a
+# a b
+# a b c
+# ASCII value of a is 97 and ASCII value of z is 122. for Capital letters, the ASCII value of A is 65 and ASCII value of Z is 90.
+    # For Small letters
+for i in range(1, 27):
+    for j in range(97, 97+i):
+        print(chr(j), end=' ')
+    print()
+
+    # For Capital letters
+for i in range(1, 27):
+    for j in range(97, 97+i):
+        print(chr(j), end=' ')
+    print()
