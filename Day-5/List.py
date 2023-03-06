93ec76946e933ee85829669aa1ef5d3ce6c6ebc5
mb-tousif <meta.block.tousif@gmail.com>
2023-03-06 13:55:55 +0600

    List

---
+++ Day-5/List.py
@@ -0,0 +1,17 @@
+# List in python
+# List is a data structure in python that is a mutable, or changeable, ordered sequence of elements. Each element or value that is inside of a list is called an item. Just as strings are defined as characters between quotes, lists are defined by having values between square brackets [ ]. Lists might contain items of different types, but usually the items all have the same type. 
+# Example:
+list3 = ["How", "are", "you", "true"]
+# Accessing Values in Lists
+# To access values in lists, use the square brackets for slicing along with the index or indices to obtain value available at that index. For example − 
+# Example: 
+list1 = ['physics', 'chemistry', 1997, 2000]
+list2 = [1, 2, 3, 4, 5, 6, 7 ]
+print ("list1[0]: ", list1[0])
+print ("list2[1:5]: ", list2[1:5])
+# Updating Lists
+# You can update single or multiple elements of lists by giving the slice on the left-hand side of the assignment operator, and you can add to elements in a list with the append() method. For example −
+# Example:
+list = ['physics', 'chemistry', 1997, 2000]
+print ("Value available at index 2 : ")
+print (list[2])
