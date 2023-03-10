c55dffa1922256e3f54cd9507e540874240277f3
mb-tousif <meta.block.tousif@gmail.com>
2023-03-11 00:18:22 +0600

    Conversion in python

---
+++ Day-6/Converting.py
@@ -0,0 +1,17 @@
+# Convert means converting a string into other data types. It can be done using int() method. It can be done using float() method. It can be done using complex() method. It can be done using ord() method. It can be done using hex() method. It can be done using oct() method. It can be done using bin() method. It can be done using list() method. It can be done using tuple() method. It can be done using set() method. It can be done using frozenset() method. It can be done using dict() method. It can be done using bool() method. It can be done using bytearray() method. It can be done using memoryview() method. 
+# Example:
+print(int("123")) # Output: 123
+print(float("123.456")) # Output: 123.456
+print(complex("123+456j")) # Output: (123+456j)
+print(ord("A")) # Output: 65
+print(hex(65)) # Output: 0x41
+print(oct(65)) # Output: 0o101
+print(bin(65)) # Output: 0b1000001
+print(list("Hello")) # Output: ['H', 'e', 'l', 'l', 'o']
+print(tuple("Hello")) # Output: ('H', 'e', 'l', 'l', 'o')
+print(set("Hello")) # Output: {'o', 'H', 'e', 'l'}
+print(frozenset("Hello")) # Output: frozenset({'o', 'H', 'e', 'l'})
+print(dict([("Hello", "World")])) # Output: {'Hello': 'World'}
+print(bool("Hello")) # Output: True
+print(bytearray("Hello", "utf-8")) # Output: bytearray(b'Hello')
+print(memoryview(bytearray("Hello", "utf-8"))) # Output: <memory at 0x000001F1D1B8F708>
